1d573d4979c03b831c2eddb81e09e11f2f28c9ed
Adrian Trunzo <adrian.trunzo@slicelife.com>
2025-07-15 21:30:46 -0800

    neovim: migrate to nvim-treesitter main branch

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,24 +1,51 @@
-local treesitter = require("nvim-treesitter.configs")
+local treesitter = require("nvim-treesitter")
 
-treesitter.setup({
-  ensure_installed = {
-    "c",
-    "comment",
-    "dockerfile",
-    "javascript",
-    "json",
-    "jsonc",
-    "lua",
-    "make",
-    "nginx",
-    "python",
-    "ruby",
-    "terraform",
-    "typescript",
-    "vimdoc",
-    "yaml",
-  },
-  highlight = {
-    enable = true
-  },
+local common = {
+  "css",
+  "html",
+  "javascript",
+  "json",
+  "jsonc",
+  "lua",
+  "python",
+  "ruby",
+  "terraform",
+  "typescript",
+  "yaml",
+}
+
+local parsers = vim.list_extend({
+  "comment",
+  "dockerfile",
+  "jsx",
+  "make",
+  "nginx",
+  "tsx",
+  "vimdoc",
+}, common)
+
+local filetypes = vim.list_extend({
+  "javascriptreact",
+  "typescriptreact",
+}, common)
+
+treesitter.install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = filetypes,
+  callback = function(event)
+    local parser = event.match
+
+    if parser == "javascriptreact" then
+      parser = "jsx"
+    end
+
+    if parser == "typescriptreact" then
+      parser = "tsx"
+    end
+
+    vim.treesitter.start(event.buf, parser)
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
 })
